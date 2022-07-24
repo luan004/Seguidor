b28b213d48b99e6d7fd167159535dc41a8805b2a
luan004 <38366327+luan004@users.noreply.github.com>
2022-07-24 19:46:32 -0300

    Add files via upload

---
+++ Codigo-SEM-PID-4.0/Codigo-SEM-PID-4.0.ino
@@ -0,0 +1,131 @@
+// -- PINOS DA PONTE H ------------
+#define ENA 10 // MOTOR A (ESQUERDO)
+#define ENB 11 // MOTOR B (DIREITO)
+#define IN1 6 // DIREÇÃO MOTOR A (ESQUERDO)
+#define IN3 8 // DIREÇÃO MOTOR B (DIREITO)
+#define IN2 7 //
+#define IN4 9 //
+
+// -- PINOS DA MATRIZ -------------
+#define NEAR A0
+#define CLP A1  
+#define S5 A2
+#define S4 A3
+#define S3 A4
+#define S2 A5
+#define S1 A6
+
+// -- ARRAY DOS SENSORES ----------
+int s[5] = {0, 0, 0, 0, 0};
+
+// -- VELOCIDADE ------------------
+int velocidade = 140;
+
+void setup(){
+  pinMode(S1, INPUT);
+  pinMode(S2, INPUT);
+  pinMode(S3, INPUT);
+  pinMode(S4, INPUT);
+  pinMode(S5, INPUT);
+  pinMode(CLP, INPUT);
+  
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+
+  Serial.begin(9600);
+
+}
+
+void loop(){
+  lerMatriz();
+  controleMotor();
+
+} 
+
+void lerMatriz(){
+  s[0] = digitalRead(S1);
+  s[1] = digitalRead(S2);
+  s[2] = digitalRead(S3);
+  s[3] = digitalRead(S4);
+  s[4] = digitalRead(S5); 
+}
+
+void controleMotor(){
+    // BOTAO CLP
+  if (digitalRead(CLP)) {
+    analogWrite(ENA, 0);
+    analogWrite(ENB, 0);
+  }
+
+
+  // -- VIRAR PARA A DIREITA ---------------------------
+
+  // 1 0 0 0 0
+  else if( (s[0] == HIGH) && (s[1] == LOW) && (s[2] == LOW) && (s[3] == LOW) && (s[4] == LOW)){
+    Serial.println("1 0 0 0 0");
+    analogWrite(ENA, velocidade - 40);
+    analogWrite(ENB, velocidade);
+  }
+  // 1 1 0 0 0
+  else if( (s[0] == HIGH) && (s[1] == HIGH) && (s[2] == LOW) && (s[3] == LOW) && (s[4] == LOW)){
+    Serial.println("1 1 0 0 0");
+    analogWrite(ENA, velocidade - 30);
+    analogWrite(ENB, velocidade);
+  }
+  // 0 1 0 0 0
+  else if((s[0] == LOW) && (s[1] == HIGH) && (s[2] == LOW) && (s[3] == LOW) && (s[4] == LOW)){
+    Serial.println("0 1 0 0 0");
+    analogWrite(ENA, velocidade - 20);
+    analogWrite(ENB, velocidade);
+  }
+  // 0 1 1 0 0
+  else if((s[0] == LOW) && (s[1] == HIGH) && (s[2] == HIGH) && (s[3] == LOW) && (s[4] == LOW)){
+    Serial.println("0 1 1 0 0");
+    analogWrite(ENA, velocidade - 10);
+    analogWrite(ENB, velocidade);
+  }
+
+
+  
+  
+  // 0 0 1 0 0 -- RETO -------------------------------------------
+  else if((s[0] == LOW) && (s[1] == LOW) && (s[2] == HIGH) && (s[3] == LOW) && (s[4] == LOW)){
+    Serial.println("0 0 1 0 0");
+    analogWrite(ENA, velocidade);
+    analogWrite(ENB, velocidade);
+  } // -------------------------------------------------------
+
+
+
+
+  // -- VIRAR PARA A DIREITA ---------------------------
+  // 0 0 1 1 0
+  else if( (s[0] == LOW) && (s[1] == LOW) && (s[2] == HIGH) && (s[3] == HIGH) && (s[4] == LOW)){
+    Serial.println("0 0 1 1 0");
+    analogWrite(ENA, velocidade);
+    analogWrite(ENB, velocidade - 10);
+  }
+  // 0 0 0 1 0
+  else if((s[0] == LOW) && (s[1] == LOW) && (s[2] == LOW) && (s[3] == HIGH) && (s[4] == LOW)){
+    Serial.println("0 0 0 1 0");
+    analogWrite(ENA, velocidade);
+    analogWrite(ENB, velocidade - 20);
+  }
+  // 0 0 0 1 1
+  else if((s[0] == LOW) && (s[1] == LOW) && (s[2] == LOW) && (s[3] == HIGH) && (s[4] == HIGH)){
+    Serial.println("0 0 0 1 1");
+    analogWrite(ENA, velocidade);
+    analogWrite(ENB, velocidade - 30);
+  }
+  // 0 0 0 0 1
+  else if((s[0] == LOW) && (s[1] == LOW) && (s[2] == LOW) && (s[3] == LOW) && (s[4] == HIGH)){
+    Serial.println("0 0 0 0 1");
+    analogWrite(ENA, velocidade);
+    analogWrite(ENB, velocidade - 40);
+  }
+
+}
